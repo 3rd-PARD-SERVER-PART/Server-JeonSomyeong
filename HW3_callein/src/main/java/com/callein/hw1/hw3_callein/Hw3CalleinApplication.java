e66a6150ea378333f14c835dbbdb8d4bc91396ac
callendev20 <callendev20@gmail.com>
2024-03-31 00:05:45 +0900

    :tada: Initial commit, HW3

---
+++ HW3_callein/src/main/java/com/callein/hw1/hw3_callein/Hw3CalleinApplication.java
@@ -0,0 +1,13 @@
+package com.callein.hw1.hw3_callein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3CalleinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw3CalleinApplication.class, args);
+    }
+
+}
