5d14534d5e2c28b52ed4e8410d7b7cd5b873ffb1
callendev20 <callendev20@gmail.com>
2024-05-05 15:27:33 +0900

    :white_check_mark: Add fifth seminar code - google login

---
+++ HW5_callein/src/main/java/com/callein/pard/hw5_callein/oauth/LoginController.java
@@ -0,0 +1,12 @@
+package com.callein.pard.hw5_callein.oauth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/loginForm")
+    public String login(){
+        return "loginForm";
+    }
+}
